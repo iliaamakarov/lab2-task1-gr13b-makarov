e65ec63a02a98662fb9a50e6d3ddd66044ee3d09
iliaamakarov <iliaamakarov@gmail.com>
2026-03-02 22:21:44 +0000

    initial frame

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
-/* main.c */
 #include <stdio.h>
+#include <stdlib.h>
 
-void main (void)
+int main(int argc, char* argv[])
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+	if (/*argc >= 1*/ 1) {
+		//const unsigned int n = argv[0];
+		const unsigned int n = 10;
+		int* arr = calloc(n, sizeof(int));
+		srand(time(NULL));
+		for (unsigned int i = 0; i < n; i++) {
+			arr[i] = rand() % 100;
+			printf("%d ", arr[i]);
+		}
+	}
+	else {
+		printf("Incorrect number of arguments.");
+		return -1;
+	}
 }
